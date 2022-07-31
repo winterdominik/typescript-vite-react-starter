901531b02e65394a529851c9bad522d1741e0ea8
Dominik Winter <dominik.winter@winter-dominik.de>
2022-07-31 16:34:37 +0200

    Add mock service worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+if (process.env.NODE_ENV === 'development') {
+  import('./mocks/browser').then( browser => browser.worker.start());
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import path from 'path';
+import fs from 'fs';
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+function removeMSW () {
+  return {
+    name: 'remove-msw',
+    resolveId (source) {
+      return source === 'virtual-module' ? source : null
+    },
+    renderStart (outputOptions, inputOptions) {
+      const outDir = outputOptions.dir
+      const msw = path.resolve(outDir, 'mockServiceWorker.js')
+      fs.rm(msw, () => console.log('Removed from build folder mockServiceWorker.js'))
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), removeMSW()]
 })
